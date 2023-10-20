67944fa7b07311fe1cad37617dae305fca558c10
ricardo <1210828@isep.ipp.+pt>
2023-10-20 18:37:44 +0100

    Adicionados schema e persistência do edifício.

---
+++ src/dataschema/IEdificioPersistence.ts
@@ -0,0 +1,10 @@
+export interface IUserPersistence {
+	_id: string;
+	codigo: string;
+	nomeOpcional: string;
+	descricao: string;
+	dimensaoMaxima: number;
+	elevadores: string;
+	pisos: string;
+    mapa: string;
+  }
---
+++ src/persistence/schemas/edificioSchema.ts
@@ -0,0 +1,55 @@
+import { IEdificioPersistence } from '../../dataschema/IEdificioPersistence';
+import mongoose from 'mongoose';
+import { Elevador } from '../../domain/elevador';
+
+const Edificio = new mongoose.Schema(
+  {
+    domainId: { 
+      type: String,
+      unique: true
+    },
+
+    codigo: {
+      type: String,
+      required: [true, 'Introduza o código do edifício'],
+      index: true,
+      unique: true
+    },
+
+    nomeOpcional: {
+      type: String,
+      required: [false, 'Please enter last name'],
+      index: true,
+    },
+
+    descricao: {
+      type: String,
+      lowercase: true,  
+      index: true,
+    },
+
+    dimensaoMaxima: {
+        type: Number,
+        required: [true, 'Introduza a dimensão máxima do piso'],
+        index: true,
+    },
+
+    elevadores: {
+        type: String,
+        index: true,
+    },
+
+    pisos: {
+        type: Array<String>,
+        index: true,
+    },
+
+    mapa: {
+        type: String,
+        index: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model<IEdificioPersistence & mongoose.Document>('Edificio', Edificio);
