e90a39a7aad95736ae3995786c78c73b93bc9fc0
Mateus Fernandes <1210821@isep.ipp.pt>
2023-10-22 17:38:12 +0100

    Atualização da classe passagemController #9

--- src/controllers/passagemController.ts
+++ src/controllers/passagemController.ts
@@ -7,35 +7,46 @@ import IPassagemService from '../services/IServices/IPassagemService';
 import IPassagemDTO from '../dto/IPassagemDTO';
 
 import { Result } from "../core/logic/Result";
-import IEdificioDTO from '../dto/IEdificioDTO';
 
 @Service()
-export default class ElevadorController implements IPassagemController /* TODO: extends ../core/infra/BaseController */ {
+export default class PassagemController implements IPassagemController /* TODO: extends ../core/infra/BaseController */ {
   constructor(
       @Inject(config.services.passagem.name) private passagemServiceInstance : IPassagemService
   ) {}
 
   public async createPassagem(req: Request, res: Response, next: NextFunction) {
+    try {
+      const passagemOrError = await this.passagemServiceInstance.createPassagem(req.body as IPassagemDTO) as Result<IPassagemDTO>;
+        
+      if (passagemOrError.isFailure) {
+        return res.status(404).send();
+      }
 
+      const passagemDTO = passagemOrError.getValue();
+      return res.status(201).json( passagemDTO );
+    }
+    catch (e) {
+      return next(e);
+    }
   };
 
   public async updatePassagem(req: Request, res: Response, next: NextFunction) {
-    
+    try {
+      const passagemOrError = await this.passagemServiceInstance.updatePassagem(req.body as IPassagemDTO) as Result<IPassagemDTO>;
+
+      if (passagemOrError.isFailure) {
+        return res.status(404).send();
+      }
+
+      const passagemDTO = passagemOrError.getValue();
+      return res.status(201).json( passagemDTO );
+    }
+    catch (e) {
+      return next(e);
+    }
   };
 
   public async listPassagens(req: Request, res: Response, next: NextFunction){
-    try {
-        const passagemOrError = await this.passagemServiceInstance.listPassagens(req.body) as Result<IPassagemDTO>;
-  
-        if (passagemOrError.isFailure) {
-          return res.status(404).send();
-        }
-  
-        const passasgemDTO = passagemOrError.getValue();
-        return res.status(200).json( passasgemDTO );
-      }
-      catch (e) {
-        return next(e);
-      }
+    null;
   }
 }
