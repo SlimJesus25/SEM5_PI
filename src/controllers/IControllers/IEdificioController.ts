c1da603aa180fcecaeb3e84da1d48c36650a0d8c
ricardo <1210828@isep.ipp.+pt>
2023-10-24 18:07:07 +0100

    Implementado controlador, serviço e DTO para listar elevadores de um edifício. Listar elevadores em edifício #14

--- src/controllers/IControllers/IEdificioController.ts
+++ src/controllers/IControllers/IEdificioController.ts
@@ -4,5 +4,4 @@ export default interface IRoleController  {
   createEdificio(req: Request, res: Response, next: NextFunction);
   updateEdificio(req: Request, res: Response, next: NextFunction);
   listElevadores(req: Request, res: Response, next: NextFunction);
-  listPassagens(req: Request, res: Response, next: NextFunction);
 }
--- src/services/IServices/IEdificioService.ts
+++ src/services/IServices/IEdificioService.ts
@@ -1,11 +1,13 @@
 import { Result } from "../../core/logic/Result";
 import IEdificioDTO from "../../dto/IEdificioDTO";
+import IListElevadoresDTO from "../../dto/IListElevadoresDTO";
+import IElevadorDTO from "../../dto/IElevadorDTO";
+
 
 export default interface IEdificioService  {
   createEdificio(edificioDTO: IEdificioDTO): Promise<Result<IEdificioDTO>>;
   updateEdificio(edificioDTO: IEdificioDTO): Promise<Result<IEdificioDTO>>;
-  listElevadores(edificioDTO: IEdificioDTO): Promise<Result<Array<IEdificioDTO>>>;
-  listPassagens(edificioDTO: IEdificioDTO): Promise<Result<Array<IEdificioDTO>>>;
+  listElevadores(codigoEdificio: IListElevadoresDTO): Promise<Result<IElevadorDTO[]>>;
 
   getEdificio (edificioId: string): Promise<Result<IEdificioDTO>>;
 }
