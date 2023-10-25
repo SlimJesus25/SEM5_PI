3044cb2b8768b199ff270210bd16869f6b39d966
Mateus Fernandes <1210821@isep.ipp.pt>
2023-10-25 22:33:08 +0100

    Atualizacao de IRoboService #18

--- src/services/IServices/IRoboService.ts
+++ src/services/IServices/IRoboService.ts
@@ -3,5 +3,4 @@ import IRoboDTO from "../../dto/IRoboDTO";
 
 export default interface IRoboService  {
   createRobo(roboDTO: IRoboDTO): Promise<Result<IRoboDTO>>;
-  updateRobo(roboDTO: IRoboDTO): Promise<Result<IRoboDTO>>;
 }
