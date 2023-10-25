4fb70bdc1a0ef58c26f47a88e89b1d4a70420b43
ricardo <1210828@isep.ipp.+pt>
2023-10-25 18:31:01 +0100

    Atualizados os parâmetros dos invocadores da criação de sala.

--- src/services/salaService.ts
+++ src/services/salaService.ts
@@ -39,11 +39,11 @@ export default class SalaService implements ISalaService {
       if(!!salaDocument)
         return Result.fail<ISalaDTO>("Já existe uma sala com o nome " + salaDocument.designacao);
 
-      const categoria = Object.keys(CategoriaSala)[Object.values(CategoriaSala).indexOf(salaDTO.categoria)];
+      const categoria = Object.keys(CategoriaSala).find(key => CategoriaSala[key] === salaDTO.categoria);
 
       const salaOrError = await Sala.create( {
         "descricaoSala": salaDTO.descricao,
-        "categoriaSala": categoria,
+        "categoriaSala": CategoriaSala[categoria],
         "designacaoSala": salaDTO.designacao
       } );
 
