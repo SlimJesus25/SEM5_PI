72e90e85077121f07aae6a7da1a0c5e642aef6d5
Mateus Fernandes <1210821@isep.ipp.pt>
2023-10-26 12:10:44 +0100

    Atualizacao IPisoController #6

--- src/controllers/IControllers/IPisoController.ts
+++ src/controllers/IControllers/IPisoController.ts
@@ -3,4 +3,5 @@ import { Request, Response, NextFunction } from 'express';
 export default interface IPisoController  {
   createPiso(req: Request, res: Response, next: NextFunction);
   updatePiso(req: Request, res: Response, next: NextFunction);
+  listPisos(req: Request, res: Response, next: NextFunction);
 }
