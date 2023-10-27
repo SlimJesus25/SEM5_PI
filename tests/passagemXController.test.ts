c6ba31ffafa7d462a4416fe4d233f433f82700c5
1210808_gabriel_silva <1210808@isep.ipp.pt>
2023-10-27 11:23:33 +0100

    Added integration test for US220

--- tests/passagemXController.test.ts
+++ tests/passagemXController.test.ts
@@ -76,7 +76,49 @@ describe('passagem controller', function () {
     });
 
 	//TODO para list pisos com passagem
-	
+	it('listPisos com passagens entre edificios', async function () {
+        let req: Partial<Request> = {};
+		req.body = {
+			"codigoEdificio": "B"
+		};
+        let res2: Partial<Response> = {};
+        let response: IPassagemDTO[] = [
+            {
+                "id": "1",
+				"designacao": "ABC",
+                "edificioOrigem": "1",
+                "edificioDestino": "2",
+                "pisoOrigem": "2",
+                "pisoDestino": "1"
+            },
+            {
+                "id": "2",
+				"designacao": "DEF",
+                "edificioOrigem": "1",
+                "edificioDestino": "2",
+                "pisoOrigem": "3",
+                "pisoDestino": "2"
+            }
+        ];
+
+        
+
+		let res: Partial<Response> = {
+			status: sinon.spy(),
+		};
+
+		let next: Partial<NextFunction> = () => {};
+
+		let passagemServiceInstance = Container.get("PassagemService");
+
+		const obj = sinon.stub(passagemServiceInstance, "listPisos").returns(Result.ok<IPassagemDTO[]>(response as IPassagemDTO[]));
+
+		const ctrl = new PassagemController(passagemServiceInstance as IPassagemService);
+		await ctrl.listPisos(<Request>req, <Response> res, <NextFunction> next);
+
+		sinon.assert.calledOnce(obj);
+		sinon.assert.calledWith(obj, sinon.match(req.body));
+    });
 	
 });
 
