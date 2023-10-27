5e2c9116bddf31c2af2d524964dadcf52643ea22
ricardo <1210828@isep.ipp.+pt>
2023-10-27 23:41:53 +0100

    Implementação dos métodos relativos a Mappers. Como gestor de frota pretendo adicionar um novo tipo de robot indicando a sua designação e que tipos de tarefas pode executar da lista prédefinida de tarefas #17

--- src/mappers/TipoRoboMap.ts
+++ src/mappers/TipoRoboMap.ts
@@ -7,23 +7,62 @@ import ITipoRoboDTO from "../dto/ITipoRoboDTO";
 
 import { UniqueEntityID } from "../core/domain/UniqueEntityID";
 import { TipoRobo } from "../domain/tipoRobo";
+import { Tarefa } from "../domain/tarefa";
+import TarefaRepo from "../repos/tarefaRepo";
+import Container from "typedi";
+
 
 // TODO implementar os métodos.
 export class TipoRoboMap extends Mapper<TipoRobo> {
   
-  public static toDTO( elevador: TipoRobo): ITipoRoboDTO {
-    return {
+  public static toDTO( tipoRobo: TipoRobo): ITipoRoboDTO {
 
+    let tarefas : string[]=[];
+
+    tipoRobo.tarefas.forEach(v => tarefas.push(v.id.toString()));
+
+    return {
+      "designacao": tipoRobo.designacao,
+      "domainId": tipoRobo.id.toString(),
+      "marca": tipoRobo.marca,
+      "modelo": tipoRobo.modelo,
+      "tarefas": tarefas
     } as ITipoRoboDTO;
   }
 
-  public static toDomain (elevador: any | Model<ITipoRoboPersistence & Document> ): TipoRobo {
-    return null;
+  public static toDomain (raw: any): TipoRobo {
+
+    let tarefas : Tarefa[]=[];
+
+    const repoTarefa = Container.get(TarefaRepo);
+
+    raw.tarefas.forEach(async tarefa => tarefas.push(await repoTarefa.findByDomainId(tarefa)));
+
+    const passagemOrError = TipoRobo.create({
+      designacao: raw.designacao,
+      marca: raw.marca,
+      modelo: raw.modelo,
+      tarefas: tarefas,
+    }, new UniqueEntityID(raw.domainId));
+
+    passagemOrError.isFailure ? console.log(passagemOrError.error) : '';
+
+    return passagemOrError.isSuccess ? passagemOrError.getValue() : null;
+
   }
 
-  public static toPersistence (elevador: TipoRobo): any {
-    return {
+  public static toPersistence (tipoRobo: TipoRobo): any {
 
+    let tarefas : string[]=[];
+
+    tipoRobo.tarefas.forEach(v => tarefas.push(v.id.toString()));
+
+    return {
+      domainId: tipoRobo.id,
+      marca: tipoRobo.marca,
+      modelo: tipoRobo.modelo,
+      designacao: tipoRobo.designacao,
+      tarefas: tarefas
     }
   }
 }
