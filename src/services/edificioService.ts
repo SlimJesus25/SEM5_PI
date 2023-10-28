70f51376f35d54098937a03511569621a13fa867
Ricardo Venâncio <ravrf@hotmail.com>
2023-10-28 15:25:28 +0100

    Corrigdo serviço de Edificio.

--- src/services/edificioService.ts
+++ src/services/edificioService.ts
@@ -4,22 +4,13 @@ import IEdificioDTO from '../dto/IEdificioDTO';
 import { Edificio } from "../domain/edificio";
 import { Piso } from "../domain/piso";
 import IEdificioRepo from '../services/IRepos/IEdificioRepo';
-import IElevadorRepo from '../services/IRepos/IElevadorRepo';
-import IPisoRepo from '../services/IRepos/IPisoRepo';
 import IMapaEdificioRepo from '../services/IRepos/IMapaEdificioRepo';
 import IEdificioService from './IServices/IEdificioService';
 import { Result } from "../core/logic/Result";
 import { EdificioMap } from "../mappers/EdificioMap";
-import { ElevadorMap } from "../mappers/ElevadorMap";
 import { CodigoEdificio } from '../domain/codigoEdificio';
-import { Elevador } from '../domain/elevador';
 import { MapaEdificio } from '../domain/mapaEdificio';
-import IListElevadoresDTO from '../dto/IListElevadoresDTO';
-import IElevadorDTO from '../dto/IElevadorDTO';
-import IPisoDTO from '../dto/IPisoDTO';
-import IListPisosDTO from '../dto/IListPisosDTO';
-import { PisoMap } from '../mappers/PisoMap';
-import IListMinMaxDTO from '../dto/IListMinMaxDTO';
+
 
 @Service()
 export default class EdificioService implements IEdificioService {
@@ -49,35 +40,21 @@ export default class EdificioService implements IEdificioService {
   public async createEdificio(edificioDTO: IEdificioDTO): Promise<Result<IEdificioDTO>> {
     try {
 
-      const edificioDocument = await this.edificioRepo.findByCodigo(edificioDTO.codigoEdificio);
+      const edificio = await this.edificioRepo.findByCodigo(edificioDTO.codigoEdificio);
 
-      let elevador: Elevador;
-
-      const elevadorOrError = await this.elevadorRepo.findByNumeroIdentificativo(edificioDTO.elevador);
-      if (elevadorOrError == null) {
-        return Result.fail<IEdificioDTO>(elevadorOrError.numeroIdentificativo);
-      }
-
-      let pisoArr: Piso[] = [];
-
-      edificioDTO.pisos.forEach(async p => pisoArr.push(await this.pisoRepo.findByDesignacao(p)));
+      if(edificio != null)
+      return Result.fail<IEdificioDTO>("Edifício com o código " + edificioDTO.codigoEdificio + " já existe!");
 
       let mapa: MapaEdificio;
-
       // Venancio: Substituir isto por algo que o identifique para além do ID.
       this.mapaRepo.findByDomainId(edificioDTO.mapaEdificio);
-      
+    
 
-      if(!!edificioDocument)
-        return Result.fail<IEdificioDTO>("Edifício com o código " + edificioDTO.codigoEdificio + " já existe!");
-
-      const edificioOrError = await Edificio.create({
+      const edificioOrError = Edificio.create({
         codigoEdificio: CodigoEdificio.create(edificioDTO.codigoEdificio).getValue(),
         descricaoEdificio: edificioDTO.descricao,
         dimensaoMaximaPiso: edificioDTO.dimensaoMaxima,
         nomeOpcionalEdificio: edificioDTO.nomeOpcional,
-        elevadores: elevador,
-        pisos: pisoArr,
         mapaEdificio: mapa
       });
 
@@ -99,21 +76,14 @@ export default class EdificioService implements IEdificioService {
   public async updateEdificio(edificioDTO: IEdificioDTO): Promise<Result<IEdificioDTO>> {
     try {
       const edificio = await this.edificioRepo.findByCodigo(edificioDTO.codigoEdificio);
-      const elevador = await this.elevadorRepo.findByNumeroIdentificativo(edificioDTO.elevador);
       const mapa = await this.mapaRepo.findByDomainId(edificioDTO.mapaEdificio); // Alterar esta pesquisa por algo que o identifique para além do ID.
-      
-      let pisos: Piso[];
-      edificioDTO.pisos.forEach(async v => pisos.push(await this.pisoRepo.findByDomainId(v)))
 
       if (edificio === null) {
         return Result.fail<IEdificioDTO>("Edificio não encontrado");
-      }
-      else {
+      }else {
         edificio.codigo = edificioDTO.codigoEdificio;
         edificio.descricao = edificioDTO.descricao;
         edificio.dimensaoMaxima = edificioDTO.dimensaoMaxima;
-        edificio.elevadores = elevador;
-        edificio.pisos = pisos;
         edificio.mapa = mapa;
         await this.edificioRepo.save(edificio);
 
