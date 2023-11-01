320033c4c9c02b4fd642dcf62cda1f927d2c456a
Mateus Fernandes <1210821@isep.ipp.pt>
2023-11-01 14:22:33 +0000

    Atualizar piso.test #1

--- tests/piso.test.ts
+++ tests/piso.test.ts
@@ -1,56 +1,33 @@
 import * as sinon from 'sinon';
 import { Edificio } from '../src/domain/edificio'
-import { error } from 'console';
-import { Elevador } from '../src/domain/elevador';
 import { Piso } from '../src/domain/piso';
 import { CodigoEdificio } from '../src/domain/codigoEdificio';
-import { Sala } from '../src/domain/sala';
-import { CategoriaSala } from '../src/domain/categoriaSala';
 
 describe('Piso', () => {
 
-    afterEach(() => {
-        sinon.restore();
-    });
+  afterEach(() => {
+    sinon.restore();
+  });
 
-   
-    /*
-    it('Piso succeeds', () =>{
-     
-        
-        const dummySala  = Sala.create({descricaoSala :"descricao", categoriaSala: CategoriaSala.laboratorio, designacaoSala: "designacao"});
-        const dummySala2  = Sala.create({descricaoSala :"descricao", categoriaSala: CategoriaSala.laboratorio, designacaoSala: "designacao2"});
+  it('Piso succeeds', () => {
 
-      const body3 = {
-        "id": "1",
-        "descricao": "gandapiso",
-        "designacao": "gandadesignacao",
-        "salas": [dummySala.getValue(), dummySala2.getValue()]
-      }
+    const dummyEdificio = Edificio.create({
+      codigoEdificio: CodigoEdificio.create("B").getValue(),
+      nomeOpcionalEdificio: "Informatica",
+      descricaoEdificio: "Grande",
+      dimensaoMaximaPiso: 100
+    }).getValue();
 
-      const piso = Piso.create(body3).getValue();
-     
+    const p = {
+      descricao: "Primeiro piso do edificio B",
+      designacao: "Piso 1",
+      edificio: dummyEdificio
+    };
 
+    const piso = Piso.create(p);
 
-        sinon.assert.match(piso.descricao, "gandapiso");
-        sinon.assert.match(piso.salas, [dummySala.getValue(), dummySala2.getValue()]);
-        sinon.assert.match(piso.designacao, "gandadesignacao");
-        
-    });
-/*
-    it('codigoEdificio not empty', () =>{
-        const body = {
-          "id": "1",
-        "dimensaoMaxima": 200,
-        "descricao": "Edificio Acolhe Malucos",
-        "nomeOpcional": "Edificio Francisco",
-        "codigoEdificio": "",
-        "elevador": "1",
-        "pisos": ["1","2","3"],
-        "mapaEdificio": "1"
-        };
-    
-        sinon.assert.throw(Edificio.create(body), "Codigo edificio vazio");
-    });
-    */
+    sinon.assert.match(piso.getValue().descricao, "Primeiro piso do edificio B");
+    sinon.assert.match(piso.getValue().designacao, "Piso 1");
+    sinon.assert.match(piso.getValue().edificio.codigo, "B");
+  });
 });
