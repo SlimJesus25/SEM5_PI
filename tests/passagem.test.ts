482e6c6ec2874037f16e437ab59fe9781313a502
1210817-Joao-Rodrigues <1210817@isep.ipp.pt>
2023-11-05 18:34:54 +0000

    adicionadas regras de negocio à passagem, e testes respetivos

--- tests/passagem.test.ts
+++ tests/passagem.test.ts
@@ -3,49 +3,52 @@ import { Edificio } from '../src/domain/edificio';
 import { CodigoEdificio } from '../src/domain/codigoEdificio';
 import { Piso } from '../src/domain/piso';
 import { Passagem } from '../src/domain/passagem';
+import { Result } from '../src/core/logic/Result';
 
 describe('Passagem', () => {
+
+
+	const dummyEdificio = Edificio.create({
+		codigoEdificio: CodigoEdificio.create("B").getValue(),
+		nomeOpcionalEdificio: "Informatica",
+		descricaoEdificio: "Grande",
+		dimensaoMaximaPiso: [100,100]
+	}).getValue();
+
+	const dummyEdificio2 = Edificio.create({
+		codigoEdificio: CodigoEdificio.create("H").getValue(),
+		nomeOpcionalEdificio: "Fisica",
+		descricaoEdificio: "Grande",
+		dimensaoMaximaPiso: [100,100]
+	}).getValue();
+
+	const dummyPiso = Piso.create({
+		"descricao": "Piso de gabinetes e aulas teórica-práticas",
+		"designacao": "B3",
+		"edificio": dummyEdificio
+	}).getValue();
+
+	const dummyPiso2 = Piso.create({
+		"descricao": "Piso de gabinetes e aulas laboratoriais",
+		"designacao": "H2",
+		"edificio": dummyEdificio2
+	}).getValue();
+
+	const p = {
+		designacao: "Passagem B para H",
+		edificioA: dummyEdificio,
+		edificioB: dummyEdificio2,
+		pisoA: dummyPiso,
+		pisoB: dummyPiso2
+
+	};
 
     afterEach(() => {
         sinon.restore();
     });
 
     it('Passagem succeeds', () => {
-	
-		const dummyEdificio = Edificio.create({
-			codigoEdificio: CodigoEdificio.create("B").getValue(),
-			nomeOpcionalEdificio: "Informatica",
-			descricaoEdificio: "Grande",
-			dimensaoMaximaPiso: [100,100]
-		}).getValue();
 
-        const dummyEdificio2 = Edificio.create({
-			codigoEdificio: CodigoEdificio.create("H").getValue(),
-			nomeOpcionalEdificio: "Fisica",
-			descricaoEdificio: "Grande",
-			dimensaoMaximaPiso: [100,100]
-		}).getValue();
-	
-		const dummyPiso = Piso.create({
-			"descricao": "Piso de gabinetes e aulas teórica-práticas",
-			"designacao": "B3",
-			"edificio": dummyEdificio
-		}).getValue();
-	
-		const dummyPiso2 = Piso.create({
-			"descricao": "Piso de gabinetes e aulas laboratoriais",
-			"designacao": "H2",
-			"edificio": dummyEdificio2
-		}).getValue();
-	
-		const p = {
-			designacao: "Passagem B para H",
-            edificioA: dummyEdificio,
-            edificioB: dummyEdificio2,
-            pisoA: dummyPiso,
-            pisoB: dummyPiso2
-	
-		};
 
         const passagem = Passagem.create(p);
 
@@ -56,4 +59,12 @@ describe('Passagem', () => {
 		sinon.assert.match(passagem.getValue().pisoB.designacao,"H2");
 
     });
+
+	it('designacao cant exceed 50 characters', () => {
+		p.designacao = "edificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificio";
+	
+		sinon.assert.match(Result.fail<Passagem>("Designação do piso excede 50 caracteres"), Passagem.create(p));
+	
+		p.designacao = "Passagem B para H";
+	  });
 });
