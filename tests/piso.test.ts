d5f6f287b9ff78aa456e72b2e3fbdd0a7040b61a
1210817-Joao-Rodrigues <1210817@isep.ipp.pt>
2023-11-05 18:29:28 +0000

    adicionadas regras de negocio e testes respetivos ao piso

--- tests/piso.test.ts
+++ tests/piso.test.ts
@@ -2,8 +2,23 @@ import * as sinon from 'sinon';
 import { Edificio } from '../src/domain/edificio'
 import { Piso } from '../src/domain/piso';
 import { CodigoEdificio } from '../src/domain/codigoEdificio';
+import { Result } from '../src/core/logic/Result';
 
 describe('Piso', () => {
+
+  const dummyEdificio = Edificio.create({
+    codigoEdificio: CodigoEdificio.create("B").getValue(),
+    nomeOpcionalEdificio: "Informatica",
+    descricaoEdificio: "Grande",
+    dimensaoMaximaPiso: [100,100]
+  }).getValue();
+
+
+  const p = {
+    descricao: "Primeiro piso do edificio B",
+    designacao: "Piso 1",
+    edificio: dummyEdificio
+  };
 
   afterEach(() => {
     sinon.restore();
@@ -11,18 +26,6 @@ describe('Piso', () => {
 
   it('Piso succeeds', () => {
 
-    const dummyEdificio = Edificio.create({
-      codigoEdificio: CodigoEdificio.create("B").getValue(),
-      nomeOpcionalEdificio: "Informatica",
-      descricaoEdificio: "Grande",
-      dimensaoMaximaPiso: [100,100]
-    }).getValue();
-
-    const p = {
-      descricao: "Primeiro piso do edificio B",
-      designacao: "Piso 1",
-      edificio: dummyEdificio
-    };
 
     const piso = Piso.create(p);
 
@@ -30,4 +33,22 @@ describe('Piso', () => {
     sinon.assert.match(piso.getValue().designacao, "Piso 1");
     sinon.assert.match(piso.getValue().edificio.codigo, "B");
   });
+
+  it('descricao cant exceed 250 characters', () => {
+    p.descricao = "edificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificio";
+
+    sinon.assert.match(Result.fail<Piso>("Descrição excede 250 carateres"), Piso.create(p));
+
+    p.descricao = "Primeiro piso do edificio B";
+  });
+
+  it('designacao cant exceed 50 characters', () => {
+    p.designacao = "edificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificio";
+
+    sinon.assert.match(Result.fail<Piso>("Designação do piso excede 50 caracteres"), Piso.create(p));
+
+    p.designacao = "Piso 1";
+  });
+
+
 });
