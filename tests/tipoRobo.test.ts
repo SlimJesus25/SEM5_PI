fdaece1d3387bdeb19702d14aa2bd23628c24d47
Mateus Fernandes <1210821@isep.ipp.pt>
2023-11-05 19:17:25 +0000

    Adicionado teste unitario tipoRobo e verificacoes #17

--- tests/tipoRobo.test.ts
+++ tests/tipoRobo.test.ts
@@ -1,6 +1,7 @@
 import * as sinon from 'sinon';
 import { Tarefa } from '../src/domain/tarefa';
 import { TipoRobo } from '../src/domain/tipoRobo';
+import { Result } from '../src/core/logic/Result';
 
 describe('TipoRobo', () => {
 
@@ -8,24 +9,24 @@ describe('TipoRobo', () => {
         sinon.restore();
     });
 
+    const dummyTarefa1 = Tarefa.create({
+        tipoTarefa: "vigilancia"
+    }).getValue();
+
+    const body = {
+        "designacao": "Polivalente",
+        "marca": "Marca 1",
+        "modelo": "Modelo 1",
+        "tarefas": [dummyTarefa1],
+    };
+
     it('TipoRobo succeeds', () => {
-
-        const dummyTarefa1 = Tarefa.create({
-            tipoTarefa: "vigilancia"
-        }).getValue();
-
-        const body = {
-            "designacao": "Polivalente",
-            "marca": "Marca 1",
-            "modelo": "Modelo 1",
-            "tarefas": dummyTarefa1,
-        };
 
         const tipoRobo = TipoRobo.create({
             designacao: body.designacao,
             marca: body.marca,
             modelo: body.modelo,
-            tarefas: [body.tarefas]
+            tarefas: body.tarefas
         }).getValue();
 
         sinon.assert.match(tipoRobo.designacao,"Polivalente");
@@ -33,4 +34,28 @@ describe('TipoRobo', () => {
         sinon.assert.match(tipoRobo.modelo,"Modelo 1");
         sinon.assert.match(tipoRobo.tarefas,[dummyTarefa1]);
     })
+
+    it('designacao cant exceed 25 characters', () => {
+        body.designacao = "edificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificio";
+    
+        sinon.assert.match(Result.fail<TipoRobo>("Designação excede 25 carateres"), TipoRobo.create(body));
+    
+        body.designacao = "Polivalente";
+      });
+    
+      it('marca cant exceed 50 characters', () => {
+        body.marca = "edificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificio";
+    
+        sinon.assert.match(Result.fail<TipoRobo>("Marca excede 50 carateres"), TipoRobo.create(body));
+    
+        body.marca = "Marca 1";
+      });
+
+      it('modelo cant exceed 100 characters', () => {
+        body.modelo = "edificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificioedificio";
+    
+        sinon.assert.match(Result.fail<TipoRobo>("Modelo excede 100 carateres"), TipoRobo.create(body));
+    
+        body.modelo = "Modelo 1";
+      });
 })
