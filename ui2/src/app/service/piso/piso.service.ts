3ded5d7f1b313c507afccd8beb35806936e10be4
Mateus Fernandes <1210821@isep.ipp.pt>
2023-11-13 12:35:00 +0000

    Adicionar metodo listar piso por edificio #30

--- ui2/src/app/service/piso/piso.service.ts
+++ ui2/src/app/service/piso/piso.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { MessageService } from '../message/message.service';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { Piso } from '../../model/piso';
 
 @Injectable({
@@ -23,25 +23,48 @@ export class PisoService {
         console.log(body);
 
         return this.http.post<Piso>(this.LogisticAPI_URL + "/createPiso", body, { 'headers': headers, observe: 'response' })
-
     }
 
+/*
+    listPisos(codigoEdificio: string) {
+        const headers = { 'content-type': 'application/json' };
+        const body = JSON.stringify(codigoEdificio);
+        console.log(body);
+
+        return this.http.get<Piso[]>(this.LogisticAPI_URL + "/listPisos", body, { 'headers': headers, observe: 'response' })
+    }
+    
+
+    listPisos(codigoEdificio: string): Observable<Piso[]> {
+        const url = `${this.LogisticAPI_URL}/listPisos`;
+        
+        // Set up headers (if necessary)
+        const headers = { 'Content-Type': 'application/json' };
+    
+        // Make the GET request with parameters in the URL
+        const pisos =  this.http.get<Piso[]>(url, { params: { codigoEdificio }, headers });
+        return pisos;
+      }
+
+*/
     //Metodo para listar pisos de um edificio
     listPisos(codigoEdificio: string): Observable<Piso[]> {
-        const listPisosEdificio = this.LogisticAPI_URL + "/listPisos?codigoEdificio=" + codigoEdificio;
-        const pisos = this.http.get<Piso[]>(listPisosEdificio)!;
-        console.log(this.LogisticAPI_URL + "/listPisos?codigoEdificio=" + codigoEdificio);
+        const listPisosEdificio = this.LogisticAPI_URL + "/listPisos/" +codigoEdificio;
+        console.log(listPisosEdificio);
+        const pisos =  this.http.get<Piso[]>(listPisosEdificio);
         return pisos;
     }
 
-    /*
-  //Metodo para listar min e max de pisos
-  listMinMax(codigoEdificio: string) : Observable<Piso[]>{
-      const body.log(body);
-      const pisos = this.http.get<Piso[]>(this.LogisticAPI_URL + "/listMinMax", body, { 'headers': headers, observe: 'response' })
-      return pisos;
-  }
-  */
+
+
+    //Metodo para listar min e max de pisos
+    listMinMax(min: string, max: string): Observable<Piso[]> {
+        const listPisosMinMax = this.LogisticAPI_URL + "/listMinMax?min=" + min+ "&max=" +max;
+        const pisos = this.http.get<Piso[]>(listPisosMinMax)!;
+        console.log(this.LogisticAPI_URL + "/listMinMax?min=" + min+ "&max=" +max);
+        return pisos;
+    }
+
 
     //Metodo para atualizar piso
     updatePiso(piso: Piso) {
