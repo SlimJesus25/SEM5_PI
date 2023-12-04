45f30fbac8d2b3327e464e053b8f5882db20c472
1210808_gabriel_silva <1210808@isep.ipp.pt>
2023-12-04 10:51:42 +0000

    Fixed a bug in create piso test on cypress

--- ui2/cypress/e2e/Gestor Campus/Piso/create-piso.cy.ts
+++ ui2/cypress/e2e/Gestor Campus/Piso/create-piso.cy.ts
@@ -7,7 +7,15 @@ describe('Creation tests for Piso', () => {
 it('should create a piso successfully', () => {
 		cy.get(':nth-child(2) > .mdc-button__label').click();
 		cy.get('#mat-menu-panel-0 > .mat-mdc-menu-content > :nth-child(2)').click();
-		
+		cy.get('.cdk-focused > .mat-mdc-menu-item-text').click();
+		cy.get(':nth-child(1) > :nth-child(2) > .ng-untouched').click();    
+		cy.get(':nth-child(1) > :nth-child(2) > .ng-untouched').type('Change Piso 2');
+		cy.get(':nth-child(2) > :nth-child(2) > .ng-untouched').click();
+		cy.get(':nth-child(2) > .ng-untouched').clear();
+		cy.get(':nth-child(2) > .ng-untouched').type('Z4');
+		cy.get('.mat-mdc-form-field-infix').click();
+		cy.get('#mat-option-4').click();
+		cy.get('.box > :nth-child(2) > :nth-child(1)').click();
 		cy.on("window:alert", (text) =>  {
 			expect(text).to.contain("Success piso creation!")
 		}) 
