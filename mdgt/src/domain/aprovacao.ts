6faf05f270d84b0b613440481bc886d8ad4413f0
ricardo <1210828@isep.ipp.+pt>
2023-12-13 14:48:16 +0000

    Modelo domínio MDGT. Classes de modelo.

---
+++ mdgt/src/domain/aprovacao.ts
@@ -0,0 +1,87 @@
+import { AggregateRoot } from "../core/domain/AggregateRoot";
+import { UniqueEntityID } from "../core/domain/UniqueEntityID";
+
+import { Result } from "../core/logic/Result";
+import { AprovacaoId } from "./aprovacaoId";
+
+import IAprovacaoDTO from "../dto/IAprovacaoDTO";
+import { Tarefa } from "./tarefa";
+import { Guard } from "../core/logic/Guard";
+
+interface AprovacaoProps {
+    estado: string;
+    requisitante: string;
+    tipoDispositivo: string;
+    tarefa: Tarefa
+}
+
+export class Aprovacao extends AggregateRoot<AprovacaoProps> {
+    get id(): UniqueEntityID {
+        return this._id;
+    }
+
+    get aprovacaoId(): AprovacaoId {
+        return new AprovacaoId(this.aprovacaoId.toValue());
+    }
+
+    get estado(): string {
+        return this.props.estado;
+    }
+
+    get requisitante(): string {
+        return this.props.requisitante;
+    }
+
+    get tipoDispositivo(): string {
+        return this.props.tipoDispositivo;
+    }
+
+    get tarefa(): Tarefa {
+        return this.props.tarefa;
+    }
+
+    set estado(value: string) {
+        this.props.estado = value;
+    }
+
+    set requisitante(value: string) {
+        this.props.requisitante = value;
+    }
+
+    set tipoDispositivo(value: string) {
+        this.props.tipoDispositivo = value;
+    }
+
+    set tarefa(value: Tarefa) {
+        this.props.tarefa = value;
+    }
+
+
+    private constructor(props: AprovacaoProps, id?: UniqueEntityID) {
+        super(props, id);
+    }
+
+    public static create(props: AprovacaoProps, id?: UniqueEntityID): Result<Aprovacao> {
+        
+        const guardedProps = [
+            { argument: props.tipoDispositivo, argumentName: 'tipoDispositivo' },
+            { argument: props.requisitante, argumentName: 'requisitante' },
+            { argument: props.estado, argumentName: 'estado' },
+            { argument: props.tarefa, argumentName: 'tarefa' }
+          ];
+      
+          const guardResult = Guard.againstNullOrUndefinedBulk(guardedProps);
+      
+          if (!guardResult.succeeded) {
+            return Result.fail<Aprovacao>(guardResult.message)
+          }     
+          else {
+            const aprovacao = new Aprovacao({
+              ...props
+            }, id);
+      
+            return Result.ok<Aprovacao>(aprovacao);
+          }
+
+    }
+}
