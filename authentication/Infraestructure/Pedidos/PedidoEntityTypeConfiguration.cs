6b71b835b7fb4f473884a25bf84823b5edaf7f1d
Mateus Fernandes <1210821@isep.ipp.pt>
2023-12-26 19:16:13 +0000

    Atualziar base de dados para garantir que campos email, phone number e nif sao uniques

--- authentication/Infraestructure/Pedidos/PedidoEntityTypeConfiguration.cs
+++ authentication/Infraestructure/Pedidos/PedidoEntityTypeConfiguration.cs
@@ -11,12 +11,30 @@ namespace RobDroneGO.Infrastructure.Pedidos
             //builder.ToTable("Users", SchemaNames.RobDroneGO);
             builder.HasKey(b => b.Id);
             builder.OwnsOne(b => b.Name).Property(b => b.Name).HasColumnName("Name").IsRequired();
-            builder.OwnsOne(b => b.Email).Property(b => b.Email).HasColumnName("Email").IsRequired();
-            builder.OwnsOne(b => b.PhoneNumber).Property(b => b.PhoneNumber).HasColumnName("PhoneNumber").IsRequired();
+            //builder.OwnsOne(b => b.Email).Property(b => b.Email).HasColumnName("Email").IsRequired();
+            builder.OwnsOne(b => b.Email, userEmailBuilder =>
+            {
+                userEmailBuilder.Property(e => e.Email).HasColumnName("Email").IsRequired();
+                userEmailBuilder.HasIndex(e => e.Email).IsUnique();
+            });
+            //builder.OwnsOne(b => b.PhoneNumber).Property(b => b.PhoneNumber).HasColumnName("PhoneNumber").IsRequired();
+            builder.OwnsOne(b => b.PhoneNumber, phoneNumberBuilder =>
+            {
+                phoneNumberBuilder.Property(e => e.PhoneNumber).HasColumnName("PhoneNumber").IsRequired();
+                phoneNumberBuilder.HasIndex(e => e.PhoneNumber).IsUnique();
+            });
             //builder.HasIndex(b => b.PhoneNumber).IsUnique();
-            builder.OwnsOne(b => b.NIF).Property(b => b.NIF).HasColumnName("NIF").IsRequired();
+            //builder.OwnsOne(b => b.NIF).Property(b => b.NIF).HasColumnName("NIF").IsRequired();
+            builder.OwnsOne(b => b.NIF, nifBuilder =>
+            {
+                nifBuilder.Property(e => e.NIF).HasColumnName("NIF").IsRequired();
+                nifBuilder.HasIndex(e => e.NIF).IsUnique();
+            });
             builder.OwnsOne(b => b.Password).Property(b => b.Password).HasColumnName("Password").IsRequired();
-            //builder.OwnsOne(b => b.Estado).Property(b => b.Estado).HasColumnName("Estado").IsRequired();
+            //builder.Property(b => b.Estado).HasColumnName("Estado").IsRequired();
+            builder.Property(b => b.Estado).HasColumnName("Estado").IsRequired();
+            builder.Property(b => b.DataPedido).HasColumnName("DataPedido").IsRequired();
+            builder.Property(b => b.DataMudancaEstado).HasColumnName("DataMudancaEstado").IsRequired();
             //builder.OwnsOne(b => b.RoleId).Property(b => b.Role.AsString).HasColumnName("Role").IsRequired();
             //builder.HasOne(b => b.RoleId) .WithMany().HasForeignKey(b => b.RoleId).IsRequired();
         }
--- authentication/Infraestructure/Users/UserEntityTypeConfiguration.cs
+++ authentication/Infraestructure/Users/UserEntityTypeConfiguration.cs
@@ -12,10 +12,27 @@ namespace RobDroneGO.Infrastructure.Users
             //builder.ToTable("Users", SchemaNames.RobDroneGO);
             builder.HasKey(b => b.Id);
             builder.OwnsOne(b => b.Name).Property(b => b.Name).HasColumnName("Name").IsRequired();
-            builder.OwnsOne(b => b.Email).Property(b => b.Email).HasColumnName("Email").IsRequired();
-            builder.OwnsOne(b => b.PhoneNumber).Property(b => b.PhoneNumber).HasColumnName("PhoneNumber").IsRequired();
-            //builder.HasIndex(b => b.PhoneNumber).IsUnique();
-            builder.OwnsOne(b => b.NIF).Property(b => b.NIF).HasColumnName("NIF").IsRequired();
+            /*builder.OwnsOne(b => b.Email).Property(b => b.Email).HasColumnName("Email").IsRequired();
+            builder.HasIndex("Email").IsUnique();*/
+            builder.OwnsOne(b => b.Email, userEmailBuilder =>
+            {
+                userEmailBuilder.Property(e => e.Email).HasColumnName("Email").IsRequired();
+                userEmailBuilder.HasIndex(e => e.Email).IsUnique();
+            });
+            /*builder.OwnsOne(b => b.PhoneNumber).Property(b => b.PhoneNumber).HasColumnName("PhoneNumber").IsRequired();
+            builder.HasIndex("PhoneNumber").IsUnique();*/
+            builder.OwnsOne(b => b.PhoneNumber, phoneNumberBuilder =>
+            {
+                phoneNumberBuilder.Property(e => e.PhoneNumber).HasColumnName("PhoneNumber").IsRequired();
+                phoneNumberBuilder.HasIndex(e => e.PhoneNumber).IsUnique();
+            });
+            /*builder.OwnsOne(b => b.NIF).Property(b => b.NIF).HasColumnName("NIF").IsRequired();
+            builder.HasIndex("NIF").IsUnique();*/
+            builder.OwnsOne(b => b.NIF, nifBuilder =>
+            {
+                nifBuilder.Property(e => e.NIF).HasColumnName("NIF").IsRequired();
+                nifBuilder.HasIndex(e => e.NIF).IsUnique();
+            });
             builder.OwnsOne(b => b.Password).Property(b => b.Password).HasColumnName("Password").IsRequired();
             //builder.OwnsOne(b => b.RoleId).Property(b => b.Role.AsString).HasColumnName("Role").IsRequired();
             //builder.HasOne(b => b.RoleId) .WithMany().HasForeignKey(b => b.RoleId).IsRequired();
