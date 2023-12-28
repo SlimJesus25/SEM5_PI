7e8670552788b658ee90b4bbc4346f86fd1894b7
Mateus Fernandes <1210821@isep.ipp.pt>
2023-12-28 17:19:15 +0000

    Atualizar scripts de asist

--- docs/use_cases/Sprint C/UC850/us850.sh
+++ docs/use_cases/Sprint C/UC850/us850.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-folder_path="/root/bin/mongodumptotal2"
+folder_path="/root/bin/mongodumptotal"
+mysql_folder_path="/root/bin/mysqldumptotal"
 log_file="/root/us850_logs.log"
 
 # Function to check if a folder follows the specified criteria
@@ -12,8 +13,8 @@ check_folder() {
     date_part=${folder_name#test_}
 
         # Check if it's the first day of the month
-        if [ "$(date -d "$date_part" +%d)" -eq 01 ]; then
-            echo "Folder '$folder_name' is a backup for the first day of the month. Keeping it." >> "$log_file"
+        if [ "$(date -d "$date_part" +%d)" -eq 28 ]; then
+            echo "Folder '$folder_name' is a backup for the 28th of the month. Keeping it." >> "$log_file"
         ((test++))
         fi
         # Check if it is the month of december and it is the first day of the week
@@ -23,12 +24,42 @@ check_folder() {
         fi
         # Check if it's the last week of the year
         if [ "$(date -d "$date_part" +%W)" -eq 52 ]; then
-         echo "Folder '$folder_name' is a backup for the last week of the year. Keeping it." >> "$log_file"
+            echo "Folder '$folder_name' is a backup for the last week of the year. Keeping it." >> "$log_file"
         ((test++))
         fi
         if [ $test -eq 0 ]; then
-          echo "Deleting folder '$folder_name' because it does not follows the specificied criteria."
+          echo "Deleting folder '$folder_name' because it does not follows the specificied criteria." >> "$log_file"
           rm -rf "$folder_path/$folder_name"
+        fi
+}
+
+
+check_sql_file() {
+    local file_name=$1
+    local test2=0
+
+    # Extract the date part
+        date_part=${file_name#sem5_pi_}
+        date_part=${date_part%.sql}
+
+        # Check if it's the first day of the month
+        if [ "$(date -d "$date_part" +%d)" -eq 28 ]; then
+            echo "File '$file_name' is a backup for the 28th of the month. Keeping it." >> "$log_file"
+        ((test2++))
+        fi
+        # Check if it is the month of december and it is the first day of the week
+        if [ "$(date -d "$date_part" +%m)" -eq 12 ] &&  [ "$(date -d "$date_part" +%u)" -eq 1 ]; then
+            echo "File '$file_name' is a backup for the first day of December. Keeping it." >> "$log_file"
+        ((test2++))
+        fi
+        # Check if it's the last week of the year
+        if [ "$(date -d "$date_part" +%W)" -eq 52 ]; then
+            echo "File '$file_name' is a backup for the last week of the year. Keeping it." >> "$log_file"
+        ((test2++))
+        fi
+        if [ $test2 -eq 0 ]; then
+          echo "Deleting file '$file_name' because it does not follows the specificied criteria." >> "$log_file"
+          rm -rf "$mysql_folder_path/$file_name"
         fi
 }
 
@@ -46,4 +77,20 @@ if [ -d "$folder_path" ]; then
     done
 else
     echo "Error: Folder $folder_path does not exist."
+fi
+
+
+# Check if the folder exists
+if [ -d "$mysql_folder_path" ]; then
+    echo "Checking files in $mysql_folder_path..."
+
+   for sql_file in "$mysql_folder_path"/*.sql; do
+        # Extract the file name
+        file_name=$(basename "$sql_file")
+
+        # Check the file against the criteria
+        check_sql_file "$file_name"
+    done
+else
+    echo "Error: Folder $mysql_folder_path does not exist."
 fi
--- docs/use_cases/Sprint C/UC930/us930.sh
+++ docs/use_cases/Sprint C/UC930/us930.sh
@@ -6,7 +6,7 @@ HOST="vsgate-s1.dei.isep.ipp.pt"
 PORT="10242"
 USERNAME="mongoadmin"
 PASSWORD="aed0452dba3a82201f874542"
-BACKUP_FOLDER="/root/bin/mongodumptotal/test_20231206"
+BACKUP_FOLDER="/root/bin/mongodumptotal/test_20231228"
 LOG_FILE="/root/us930_logs.log"
 
 # MongoDB query
@@ -19,4 +19,20 @@ if mongorestore --host "$HOST" --port "$PORT" --username "$USERNAME" --password 
         echo "Mongorestore success" >> "$LOG_FILE"
 else
         echo "Mongorestore failed" >> "$LOG_FILE"
+fi
+
+
+#MySql connection details
+MYSQL_USER="root"
+MYSQL_HOST="vs1220.dei.isep.ipp.pt"
+DATABASE_NAME="sem5_pi"
+MYSQL_PASSWORD="M/ISRo5Wvz33"
+DUMP_FILE="/root/bin/mysqldumptotal/sem5_pi_20231228.sql"
+
+if mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$DATABASE_NAME" < "$DUMP_FILE" >> "$LOG_FILE" 2>&1; then
+    result=$(mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$DATABASE_NAME" -e "SELECT * FROM Pedidos")
+    echo "Query result: $result" >> "$LOG_FILE"
+    echo "MySql restore success" >> "$LOG_FILE"
+else
+    echo "MySql restore failed" >> "$LOG_FILE"
 fi
