d1c8ede5c1fb3e4abbec0fed8de2bb04bf632f85
1210808_gabriel_silva <1210808@isep.ipp.pt>
2023-12-29 23:55:33 +0000

    G55-46 Updated cypress tests

--- ui2/cypress/e2e/Gestor Tarefas/Tarefas/listarTarefas.cy.ts
+++ ui2/cypress/e2e/Gestor Tarefas/Tarefas/listarTarefas.cy.ts
@@ -5,15 +5,32 @@ describe('List tests for Tarefas Nao Aprovadas', () => {
 	});
 	
 	it('should List Tarefas by Estado', () => {
-		
+		cy.get(':nth-child(12) > .mdc-button__label').click();
+		cy.get('.mat-mdc-menu-content > .mat-mdc-menu-trigger').click();
+		cy.get('#mat-menu-panel-9 > .mat-mdc-menu-content > :nth-child(3)').click();
+		cy.get('.box > :nth-child(1)').click();
+		cy.get('.mat-mdc-select-placeholder').click();
+		cy.get('#mat-option-2 > .mdc-list-item__primary-text').click();
+		cy.get('button.ng-tns-c1798928316-16').click();
 	});
 	
 	it('should List Tarefas by Utente', () => {
-	
+		cy.get(':nth-child(12) > .mdc-button__label').click();
+		cy.get('.mat-mdc-menu-trigger > .mat-mdc-menu-item-text').click();
+		cy.get('#mat-menu-panel-9 > .mat-mdc-menu-content > :nth-child(3) > .mat-mdc-menu-item-text').click();
+		cy.get('.box > :nth-child(2)').click();
+		cy.get('#mat-input-0').type('joao@isep.ipp.pt');
+		cy.get('button.ng-tns-c1798928316-16').click();
 	});
 	
 	it('should List Tarefas by Tipo Dispositivo', () => {
-		
+		cy.get(':nth-child(12) > .mdc-button__label').click();
+		cy.get('.mat-mdc-menu-trigger > .mat-mdc-menu-item-text').click();
+		cy.get('#mat-menu-panel-9 > .mat-mdc-menu-content > :nth-child(3) > .mat-mdc-menu-item-text').click();
+		cy.get('.box > :nth-child(3)').click();
+		cy.get('#mat-input-0').type('Polivalente');
+		cy.get('button.ng-tns-c1798928316-16').click();
+
 	});
 	
 	});
--- ui2/cypress/e2e/login-page.cy.ts
+++ ui2/cypress/e2e/login-page.cy.ts
@@ -3,21 +3,19 @@ describe('Login Page', () => {
 	  cy.visit('http://localhost:4200/login'); // Adjust the path if needed
 	});
   
-	it('should login with valid password', () => {
-	  // Type a valid username and password
-	  cy.login()
-	
+	it('should login an user successfully', () => {
+		cy.get('[type="text"]').type('taf2@isep.ipp.pt');
+		cy.get('.ng-pristine').click();
+		cy.get('.ng-untouched').type('Teste123456@');
+		cy.get('button').click();
+		cy.url().should('include', '/mainMenu');
 	});
   
 	it('should not login with invalid password', () => {
-	  cy.get('input[name="username"]').type('invalid-username');
-	  cy.get('input[name="password"]').type('invalid-password');
-  
-	  const loginButton = cy.get('button')
-	  loginButton.click();
-	  
-	  cy.url().should('not.include', '/mainMenu');
-  
-	  // You can also assert other elements or messages indicating a failed login
+		cy.get('[type="text"]').type('taf2@isep.ipp.pt');
+		cy.get('.ng-pristine').click();
+		cy.get('.ng-untouched').type('Teste123456!');
+		cy.get('button').click();
+		cy.url().should('not.include', '/mainMenu');
 	});
   });
