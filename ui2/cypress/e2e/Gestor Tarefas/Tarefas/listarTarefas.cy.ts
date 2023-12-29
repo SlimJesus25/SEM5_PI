867a1923fb28881a364b8b2a720b2f1f2272697d
1210808_gabriel_silva <1210808@isep.ipp.pt>
2023-12-29 17:22:28 +0000

    G55-40 Added cypress tests for the US

---
+++ ui2/cypress/e2e/Gestor Tarefas/Tarefas/listarTarefas.cy.ts
@@ -0,0 +1,19 @@
+import "../../../support/commands"
+describe('List tests for Tarefas Nao Aprovadas', () => {
+	beforeEach(() => {
+		cy.loginGestorTarefa()
+	});
+	
+	it('should List Tarefas by Estado', () => {
+		
+	});
+	
+	it('should List Tarefas by Utente', () => {
+	
+	});
+	
+	it('should List Tarefas by Tipo Dispositivo', () => {
+		
+	});
+	
+	});
