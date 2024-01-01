3061569f58417ff58a561e0979e0fee0e16c13b9
1210808_gabriel_silva <1210808@isep.ipp.pt>
2024-01-01 23:38:30 +0000

    G55-38 Updated alert to snackbar

--- ui2/src/app/component/user/registarUser/registar-user.component.ts
+++ ui2/src/app/component/user/registarUser/registar-user.component.ts
@@ -4,6 +4,7 @@ import { MessageService } from '../../../service/message/message.service';
 import { UserService } from '../../../service/user/user.service';
 import { RoleService } from '../../../service/role/role.service';
 import { Role } from '../../../model/role';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-registar-user',
   templateUrl: './registar-user.component.html',
@@ -17,7 +18,8 @@ export class RegistarUserComponent implements OnInit {
   constructor(
     private location: Location,
     private userService: UserService,
-    private roleService : RoleService
+    private roleService : RoleService,
+    private snackBar: MatSnackBar
   ) { 
     this.roleService.getAllRoles().subscribe(roles => this.roles = roles.filter(role => role.name !== 'Utente'));
   }
@@ -35,17 +37,26 @@ export class RegistarUserComponent implements OnInit {
     let errorOrSuccess: any = this.userService.criarUser(this.user);
     errorOrSuccess.subscribe(
       (data: any) => {
-        alert("User criado");
+        this.showNotification("User criado");
       },
 
       (error: any) => {
-        alert("Falha na criação do User!");
+        this.showNotification("Falha na criação do User!");
       }
     );
 
     return errorOrSuccess;
   }
-
+  
+  showNotification(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000, // Adjust the duration as needed
+      horizontalPosition: 'center', // Position of the snackbar
+      verticalPosition: 'top',
+      panelClass: ['snackbar-success', 'mat-elevation-z6'], // Optional: Add custom styling classes
+    });
+  }
+  
   goBack(): void {
     this.location.back();
   }
