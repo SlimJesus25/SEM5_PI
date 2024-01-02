c6e94821b9d5373f89f75ddda2489773f42d4b96
1210808_gabriel_silva <1210808@isep.ipp.pt>
2024-01-02 21:35:23 +0000

    G55-41 Updated service of the US

--- ui2/src/app/service/sala/sala.service.ts
+++ ui2/src/app/service/sala/sala.service.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { MessageService } from '../message/message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Sala } from '../../model/sala';
+import { Piso } from '../../model/piso';
 
 @Injectable({
   providedIn: 'root'
@@ -25,14 +26,21 @@ export class SalaService {
     return this.http.post<Sala>(this.LogisticAPI_URL + "/createSala", body, {'headers':headers , observe: 'response'})
 
   }
+  
+  getSalasPiso(pisoInicial: string): Observable<Sala[]> {
+    //const headers = {'authorization': 'Bearer '+ localStorage.getItem("token")};
+    const body = {pisoInicial}
+    const salas = this.http.get<Sala[]>(this.LogisticAPI_URL+ "/listSalasPiso" + pisoInicial);
 
+    return salas;
+  }
+  
   getSalas(): Observable<Sala[]> {
     //const headers = {'authorization': 'Bearer '+ localStorage.getItem("token")};
     const salas = this.http.get<Sala[]>(this.LogisticAPI_URL+ "/listSalas");
 
     return salas;
   }
-
 
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
